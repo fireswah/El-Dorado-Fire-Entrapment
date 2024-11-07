dc403003312cf7edab77bc6908f82712809d5391
fireswah <52438028+fireswah@users.noreply.github.com>
2024-11-07 09:05:55 -0700

    Update standdisplay.js
    Remove textures and add emissive.  Adjustments to scaling text.

--- scripts/standdisplay.js
+++ scripts/standdisplay.js
@@ -3,14 +3,10 @@
 	-Creates a stand podium for the El Dorado Museum
     -Creates the billboard stand # display above the podium
     -Adds graphics, text and/or play button to the podium for use with the content
-    -Requires separate audio player component
 */
 AFRAME.registerComponent('stand-display', {
 	schema: {
-        basemat: { type: 'string', default: '' },
-        platemat: { type: 'string', default: '' },
         platetext: { type: 'string', default: '' },
-        billboardmat: { type: 'string', default: '' },
         billboardtext: { type: 'string', default: '' },
         audiosrc: { type: 'string', default: '' }
 	},
@@ -30,12 +26,14 @@ AFRAME.registerComponent('stand-display', {
         base.setAttribute( 'radius-top', 0.06 );
         base.setAttribute( 'height', 0.8 );
         base.setAttribute( 'segments-radial', 6 );
-        base.setAttribute( 'src', data.basemat );
+        base.setAttribute( 'material', 'color', 'gray' );
+        base.setAttribute( 'material', 'emissive', 'gray' );
+        base.setAttribute( 'material', 'emissiveIntensity', 0.5 );
         el.appendChild( base );
 
         //Create faceplate, plate text and button
         var plate = document.createElement( 'a-box' );
-        var plttext = document.createElement( 'a-text' );
+        var plttext = document.createElement( 'a-entity' );
         var button = document.createElement( 'a-cylinder' );
         
         plate.setAttribute( 'height', 0.288 );
@@ -43,21 +41,28 @@ AFRAME.registerComponent('stand-display', {
         plate.setAttribute( 'depth', 0.05 );
         plate.setAttribute( 'position', '0 0.9 0' );
         plate.setAttribute( 'rotation', '-25 0 0' );
-        plate.setAttribute( 'src', data.platemat );
+        plate.setAttribute( 'material', 'color', '#5c4033' );
+        plate.setAttribute( 'material', 'emissive', '#5c4033' );
+        plate.setAttribute( 'material', 'emissiveIntensity', 0.5 );
         
-        plttext.setAttribute( 'position', '0.526 0 0.026' );
+        //using an entity should autoscale the width of text to the primitive: https://aframe.io/docs/1.6.0/components/text.html
+        plttext.setAttribute( 'geometry', 'primitive', 'plane' );
+        plttext.setAttribute( 'geometry', 'width', 0.85 );
+        plttext.setAttribute( 'geometry', 'height', 0.25 );
+        plttext.setAttribute( 'position', '0.137 0 0.026' );
+        plttext.setAttribute( 'material', 'opacity', 0.0 );
         plttext.setAttribute( 'text', 'value', data.platetext );
-        plttext.setAttribute( 'text', 'width', 0.768 );
-        plttext.setAttribute( 'scale', '2.5 4 1' );
-        plttext.setAttribute( 'text', 'align', 'right' );
+        plttext.setAttribute( 'text', 'align', 'center' );
         plttext.setAttribute( 'text', 'color', 'silver' );
         
         button.setAttribute( 'id', idName + '_button' );
         button.setAttribute( 'class', 'button' );
-        button.setAttribute( 'position', '-0.384 0 0' );
+        button.setAttribute( 'position', '-0.385 -0.008 0.018' );
         button.setAttribute( 'rotation', '90 0 0' );
-        button.setAttribute( 'scale', '0.075 0.075 0.075' );
+        button.setAttribute( 'scale', '0.075 0.045 0.075' );
         button.setAttribute( 'material', 'color', 'red' );
+        button.setAttribute( 'material', 'emissive', 'red' );
+        button.setAttribute( 'material', 'emissiveIntensity', 0.5 );
         
         el.appendChild( plate );
         plate.appendChild( plttext );
@@ -66,7 +71,7 @@ AFRAME.registerComponent('stand-display', {
         button.addEventListener( 'click', function () {
             if( button.getAttribute( 'sound' ) ) {
                 //don't reload over again.
-                console.log('nothing');
+                console.log( 'This sound file has already loaded.');
             }else {
                 button.setAttribute( 'sound', {
                     src: data.audiosrc,
@@ -75,26 +80,38 @@ AFRAME.registerComponent('stand-display', {
                     maxDistance: 20,
                     distanceModel: 'linear'
                 });
-                console.log( 'loading sound' );
+                //console.log( 'loading sound' );
             }
-            
         });
         
+        //Once sound loads, change button color for visual indicator
         button.addEventListener( 'sound-loaded', function () {
-			//document.getElementById( data.id ).load();
             button.setAttribute( 'material', 'color', 'green' );
+            button.setAttribute( 'material', 'emissive', 'green' );
+            button.setAttribute( 'material', 'emissiveIntensity', 0.7 );
 		});
 
         //Create billboard and billboard text
-        var billboard = document.createElement( 'a-plane' );
+        //Note the text on the billboard doesn't autoscale, it should remain the same throughout.
+        var billboard = document.createElement( 'a-entity' );
+        var top = document.createElement( 'a-plane' );
+        var pointer = document.createElement( 'a-triangle' );
         var bbtext = document.createElement( 'a-text' );
-        billboard.setAttribute( 'rotation', '0 0 0' );
-        billboard.setAttribute( 'width', 0.900 );
-        billboard.setAttribute( 'height', 2.080 );
-        billboard.setAttribute( 'position', '0 2.5 0' );
-        billboard.setAttribute( 'src', data.billboardmat );
-        billboard.setAttribute( 'material', 'side', 'double' );
-        billboard.setAttribute( 'material', 'alphaTest', 0.5 );
+
+        top.setAttribute( 'width', 0.900 );
+        top.setAttribute( 'height', 2.080 );
+        top.setAttribute( 'rotation', '0 0 0' );
+        top.setAttribute( 'position', '0 2.5 0' );
+        top.setAttribute( 'material', 'side', 'double' );
+        top.setAttribute( 'material', 'emissive', 'yellow' );
+        top.setAttribute( 'material', 'emissiveIntensity', 0.5 );
+        pointer.setAttribute( 'rotation', '0 0 180' );
+        pointer.setAttribute( 'scale', '0.25 0.25 1.0' );
+        pointer.setAttribute( 'position', '0 1.335 0' );
+        pointer.setAttribute( 'material', 'side', 'double' );
+        pointer.setAttribute( 'material', 'emissive', 'yellow' );
+        pointer.setAttribute( 'material', 'emissiveIntensity', 0.5 );
+        
         billboard.setAttribute( 'animation', {
             property: 'rotation',
             to: '0 360 0',
@@ -108,10 +125,10 @@ AFRAME.registerComponent('stand-display', {
         bbtext.setAttribute( 'text', 'color', 'black' );
         bbtext.setAttribute( 'position', '0 0.3 0.01' );
         
+        billboard.appendChild( top );
+        billboard.appendChild( pointer );
+        top.appendChild( bbtext );
         el.appendChild( billboard );
-        billboard.appendChild( bbtext );
-
-
 	},
 
 	update: function () {},
