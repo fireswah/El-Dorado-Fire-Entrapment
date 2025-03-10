5a367aca4a971cc7fabef6fc4a1e9b8ba0c8e4df
fireswah <52438028+fireswah@users.noreply.github.com>
2025-03-10 09:01:43 -0600

    Update href-target.js
    Add (c) info

--- scripts/href-target.js
+++ scripts/href-target.js
@@ -1,6 +1,21 @@
 /*
-Component to open href links with a target of a new tab, rather than traversing with the same tab.
-Intent is to keep the user in the same scene and providing the linked resource in a separate tab.
+	Copyright 2025 Matt Gibson
+        href target component:
+		-Component to open href links with a target of a new tab, rather than traversing with the same tab.
+		-Intent is to keep the user in the same scene and providing the linked resource in a separate tab.
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 AFRAME.registerComponent('href-target', {
 	schema: {
