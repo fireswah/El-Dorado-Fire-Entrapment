91e6cf8cb25d629a2089aab5109d9a0799947c62
fireswah <52438028+fireswah@users.noreply.github.com>
2025-03-10 09:08:37 -0600

    Update visdistance.js
    Add (c) info

--- scripts/visdistance.js
+++ scripts/visdistance.js
@@ -1,5 +1,20 @@
 /*
-Component to toggle visibility on content given user's position.
+	Copyright 2025 Matt Gibson
+        LOD type visibile toggle component:
+		-Component to toggle visibility on content given user's position.
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 AFRAME.registerComponent('visdistance', {
 	schema: {
