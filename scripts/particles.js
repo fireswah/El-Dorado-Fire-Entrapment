9da55e535642236799a3a676658fdd9fcf0fe18e
fireswah <52438028+fireswah@users.noreply.github.com>
2025-03-10 09:03:39 -0600

    Update particles.js
    Add (c) info

--- scripts/particles.js
+++ scripts/particles.js
@@ -1,7 +1,21 @@
 /*
-Basic particle system that can handle rotation around center, "drift" along with color change and blend modes
-required for CRVP displays
-Very thankful for SimonDev's tutorial @ https://www.youtube.com/watch?v=OFqENgtqRAY
+	Copyright 2025 Matt Gibson
+        Basic particle system component:
+		-Can handle rotation around center, "drift" along with color change and blend modes required for CRVP displays
+		-Very thankful for SimonDev's tutorial @ https://www.youtube.com/watch?v=OFqENgtqRAY
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 AFRAME.registerComponent('particle_system', {
 	schema: {
